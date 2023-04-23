a153690e46f9621b69109d7fe6e5a12dca37ae31
FortniteGamer696 <120283785+FortniteGamer696@users.noreply.github.com>
2023-04-23 21:11:07 +0200

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -367,7 +367,7 @@ do
             Max = 2, Current = 0
         }
         --
-        Library.Theme.Logo = Utility.AddImage("akiri/Assets/UI/Logo2.png", "https://i.imgur.com/i6LNYCZ")
+        Library.Theme.Logo = Utility.AddImage("akiri/Assets/UI/Logo2.png", "https://i.imgur.com/i6LNYCZ_d.png")
         --
         local WindowOutline = Utility.AddDrawing("Square", {
             Size = WindowSize,
